89c563307c805b8bde7a10295032ce146c5bd0e9
ericyn <ericnoel187@gmail.com>
2022-08-16 20:41:31 +0400

    p5js audio added

--- sketch.js
+++ sketch.js
@@ -1,12 +1,17 @@
+let sound;
+let audio;
+
+function preload() {
+    sound = loadSound('./audio/MONTE_NFT_808.wav');
+    audio = loadSound('./audio/monte_booker_-_endless.wav');
+}
+
 function setup() {
     createCanvas(400, 400);
+    background(0);
+    audio.setVolume(1);
+    audio.stop();
 }
   
 function draw() {
-    if (mouseIsPressed) {
-      fill(0);
-    } else {
-      fill(255);
-    }
-    ellipse(mouseX, mouseY, 80, 80);
 }
